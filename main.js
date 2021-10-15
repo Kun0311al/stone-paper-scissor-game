6e550e8f873105444682f73a9a3e12e45c7e28d2
kunal parkar <83920024+Kun0311al@users.noreply.github.com>
2021-10-15 23:28:14 +0530

    adding javascript

---
+++ main.js
@@ -0,0 +1,56 @@
+const usercore = 0;
+const compScore = 0;
+const userScore_span = document.getElementById("User-score");
+const compScore_span = document.getElementById("Comp-score");
+const scoreBoard_div = document.querySelector(".score-board");
+const result_div = document.querySelector(".result");
+const rock_div = document.getElementById("r");
+const paper_div = document.getElementById("p");
+const scissor_div = document.getElementById("s");
+
+function getCompChoice(){
+    const choices = ['r','p','s'];
+    const randomNumber = Math.floor(Math.random()*3);
+    return choices[randomNumber];
+}
+
+function game(userChoice){
+    const compChoice = getCompChoice();
+    console.log("user choice =>"+userChoice);
+    console.log("comp choice =>"+compChoice);
+    switch(userChoice+compChoice){
+        case"pr":
+        case"sp":
+        case"rs":
+            console.log("User Win");
+        break;
+        case"rp":
+        case"ps":
+        case"sr":
+            console.log("Computer Win");
+        break;
+        case"ss":
+        case"rr":
+        case"pp":
+            console.log("Draw");
+        break;
+    }
+}
+
+function main(){
+    rock_div.addEventListener('click',function(){
+        console.log("it's rock");
+        game("r");
+    })
+    
+    paper_div.addEventListener('click',function(){
+        console.log("it's paper");
+        game("p");
+    })
+    
+    scissor_div.addEventListener('click',function(){
+        console.log("it's scissor");
+        game("s");
+    })
+}
+main();
